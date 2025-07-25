7e76caa8c30fb96a76d0cb6d4f0e1c70e5cef76a
SUMIT KUMAR <infosumitkumar3322@gmail.com>
2025-07-25 19:26:37 +0530

    update MongoDB connection URI and simplify connection options

--- config/db.js
+++ config/db.js
@@ -2,13 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+   await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected ✅');
   } catch (err) {
-    console.error(err.message);
+    console.error("mongo error: " + err.message);
     process.exit(1);
   }
 };
